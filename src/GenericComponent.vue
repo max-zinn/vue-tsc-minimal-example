3dab5c26729cf59baf62c59a45b653b02a20bc2a
Maximilian Zinn <maximilian.zinn-external@dachser.com>
2024-02-08 08:55:07 +0100

    Initial commit

---
+++ src/GenericComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="item of items" :key="`${item}`">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+<script setup lang="ts" generic="T">
+interface Props {
+  items: T[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  items: () => [],
+});
+const modelValue = defineModel<T>();
+</script>
+
+<style scoped lang="scss"></style>
---
+++ src/GenericComponent_working-example.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="item of items" :key="`${item}`">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+<script setup lang="ts" generic="T">
+interface Props {
+  items: T[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  items: () => [],
+});
+/**
+ * When using `defineModel` Generic type T cannot be used, else the props above won't be recognized
+ */
+const modelValue = defineModel<string>();
+</script>
+
+<style scoped lang="scss"></style>
